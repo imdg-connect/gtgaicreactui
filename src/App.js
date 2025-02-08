49b5ba99777c0b4eaaad2c58c1abe7283837cf9d
dgprofile <90986919+dgprofile@users.noreply.github.com>
2025-02-07 21:08:36 -0500

    0207-2 commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return <h1>Hello, World!</h1>;
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('https://wappgaicbackend-epd3c4cyh0e3ergw.eastus-01.azurewebsites.net/hello')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
 }
 
 export default App;
